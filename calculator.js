87cf1ad810537a4d719f9c2ec2aca2b753c70232
Elisha Adeboye <y.adeboye@gmail.com>
2022-06-22 13:52:47 +0100

    refinement

--- calculator.js
+++ calculator.js
@@ -18,24 +18,24 @@ let wrong = "Wrong input"
 
 function calculation(){
     let answer;
-    let opera = parseFloat(prompt("Enter 1 for addition , 2 for subtraction , 3 for multiplication , 4 for division"), 10);
     let num1 = parseFloat(prompt("Enter the first number"), 10);
+    let opera = prompt("Enter operator e.g + * - /");
     let num2 = parseFloat(prompt("Enter second number "), 10);
 
 
-     if(opera == 1){
+     if(opera == "+"){
         answer = add(num1, num2);
         document.getElementById("answer").innerHTML = answer;
      }
-     else if(opera == 2){ 
+     else if(opera == "-"){ 
         answer = sub(num1, num2)
         document.getElementById("answer").innerHTML = answer;
      }
-     else if(opera == 3){
+     else if(opera == "*"){
         answer = mult(num1, num2)
         document.getElementById("answer").innerHTML = answer;
      }
-     else if(opera == 4){
+     else if(opera == "/"){
         answer = div(num1, num2)
         document.getElementById("answer").innerHTML = answer;
      }
